48362663a58ae2b4ced7ed2cb056d355aee46d90
Bakare Opeyemi <bacray297@gmail.com>
2024-05-16 13:03:17 +0100

    Corrected typos

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def load_models():
 
 gru_model,lstm_model,cnn_model,transformer_model = load_models()
 
-prediction_decoding = {0:"medical doctor", 1:"veterinarian", 2:"person who is neither a medical doctor not a veterinarian" }
+prediction_decoding = {0:"medical doctor", 1:"veterinarian", 2:"person who is neither a medical doctor nor a veterinarian" }
 
 def main():
     st.title('Social Media Comment Classification')
@@ -36,7 +36,7 @@ def main():
     html_temp2 = """
     <div style="background:#14302f ;padding:10px">
     <p style="color:white;text-align:left;">This project aims to infer the profession of reddit users from their comments </p>
-    <h5 style="color:white;text-align:left;">Class Codes</h5>
+    <p style="color:white;text-align:left;"><b>Class Codes</b><p>
     <p style="color:white;text-align:left;">Medical Practitioner: 0, Veterinarian: 1, Others:2 </p>
     </div>
     """
